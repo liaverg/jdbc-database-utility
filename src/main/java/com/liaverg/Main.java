3786330f6aca5fef2977af337e5157fe1f5ce609
Lia Vergou <lia.vergou@gmail.com>
2023-11-13 15:41:40 +0200

    Create Main

--- src/main/java/com/liaverg/Main.java
+++ src/main/java/com/liaverg/Main.java
@@ -1,7 +1,31 @@
 package com.liaverg;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+
 public class Main {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/mydb";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PGSimpleDataSource pg_dataSource = new PGSimpleDataSource();
+        pg_dataSource.setURL(DB_URL);
+        pg_dataSource.setUser(USER);
+        pg_dataSource.setPassword(PASSWORD);
+        DbUtils dbUtils = new DbUtils(pg_dataSource);
+
+        DbUtils.ConnectionConsumer insertData = conn -> {
+            String insertSQL = "INSERT INTO users_directory.users (username, email) VALUES (?, ?)";
+            try (PreparedStatement insertStmt = conn.prepareStatement(insertSQL)) {
+                insertStmt.setString(1, "john_doe");
+                insertStmt.setString(2, "john.doe@example.com");
+                insertStmt.executeUpdate();
+            }
+        };
+
+        DbUtils.executeStatements(insertData);
     }
 }
