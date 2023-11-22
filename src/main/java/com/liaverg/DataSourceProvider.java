dec3879655c55c1cc45b8dcfef636a3a3f26d094
Lia Vergou <lia.vergou@gmail.com>
2023-11-22 15:24:39 +0200

    Get properties from database.properties file

--- src/main/java/com/liaverg/DataSourceProvider.java
+++ src/main/java/com/liaverg/DataSourceProvider.java
@@ -3,11 +3,21 @@ package com.liaverg;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
 
 public class DataSourceProvider {
     private final String url;
     private final String username;
     private final String password;
+
+    public DataSourceProvider() {
+        PropertiesReader propertiesReader = new PropertiesReader();
+        this.url = propertiesReader.getPropertiesDbUrl();
+        this.username = propertiesReader.getPropertiesUser();
+        this.password = propertiesReader.getPropertiesPassword();
+    }
 
     public DataSourceProvider(String url, String username, String password) {
         this.url = url;
@@ -15,7 +25,7 @@ public class DataSourceProvider {
         this.password = password;
     }
 
-    public DataSource createDataSource(){
+    public DataSource createDataSource() {
         PGSimpleDataSource pg_dataSource = new PGSimpleDataSource();
         pg_dataSource.setURL(url);
         pg_dataSource.setUser(username);
