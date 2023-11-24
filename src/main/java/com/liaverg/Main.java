59d4d122fb4e00e3804bf400d418189ee4789b57
Lia Vergou <lia.vergou@gmail.com>
2023-11-24 12:54:55 +0200

    Cleaner lambda expressions in Main

--- src/main/java/com/liaverg/Main.java
+++ src/main/java/com/liaverg/Main.java
@@ -3,49 +3,52 @@ package com.liaverg;
 import com.liaverg.DbUtils.ConnectionConsumer;
 import com.liaverg.DbUtils.ConnectionFunction;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 
 public class Main {
-    private static final ConnectionConsumer insertData = conn -> {
+    private static void insertUser(Connection conn, String username, String email) throws SQLException {
         String insertSQL = "INSERT INTO users_directory.users (username, email) VALUES (?, ?)";
         try (PreparedStatement insertStatement = conn.prepareStatement(insertSQL)) {
-            insertStatement.setString(1, "john_doe");
-            insertStatement.setString(2, "john.doe@example.com");
-            insertStatement.executeUpdate();
-            insertStatement.setString(1, "jane_doe");
-            insertStatement.setString(2, "jane.doe@example.com");
+            insertStatement.setString(1, username);
+            insertStatement.setString(2, email);
             insertStatement.executeUpdate();
         }
+    }
+    private static final ConnectionConsumer insertUsers = conn -> {
+        insertUser(conn, "john_doe", "john.doe@example.com");
+        insertUser(conn, "jane_doe", "jane.doe@example.com");
     };
 
-    private static final ConnectionConsumer insertDataWithNestedTransaction = conn -> {
-        DbUtils.executeStatementsInTransaction(insertData);
-        String insertSQL = "INSERT INTO users_directory.users (username, email) VALUES (?, ?)";
-        try (PreparedStatement insertStatement = conn.prepareStatement(insertSQL)) {
-            insertStatement.setString(1, "jake_doe");
-            insertStatement.setString(2, "jake.doe@example.com");
-            insertStatement.executeUpdate();
-            //throw new SQLException("Simulated exception during statement execution");
-        }
+    private static final ConnectionConsumer insertUsersWithNestedTransaction = conn -> {
+        DbUtils.executeStatementsInTransaction(connection -> {
+            insertUser(connection, "john_doe", "john.doe@example.com");
+            insertUser(connection, "jane_doe", "jane.doe@example.com");
+        });
+        insertUser(conn, "jake_doe", "jake.doe@example.com");
+        //throw new SQLException("Simulated exception during statement execution");
     };
 
-    private static final ConnectionFunction updateData = conn -> {
+    private static int updateUser(Connection conn, String username, String email) throws SQLException {
         String updateSQL = "UPDATE users_directory.users SET email = ? WHERE username = ?";
         try (PreparedStatement updateStatement = conn.prepareStatement(updateSQL)) {
-            updateStatement.setString(1, "john.doe@gmail.com");
-            updateStatement.setString(2, "john_doe");
-            int updateCount = updateStatement.executeUpdate();
-            updateStatement.setString(1, "jane.doe@gmail.com");
-            updateStatement.setString(2, "jane_doe");
-            updateCount += updateStatement.executeUpdate();
-            return updateCount;
+            updateStatement.setString(1, email);
+            updateStatement.setString(2, username);
+            return updateStatement.executeUpdate();
         }
+    }
+
+    private static final ConnectionFunction updateUserEmails = conn -> {
+        int updateCount = 0;
+        updateCount += updateUser(conn, "john.doe@gmail.com","john_doe");
+        updateCount += updateUser(conn, "jane.doe@gmail.com","jane_doe");
+        return updateCount;
     };
 
-    private static final ConnectionFunction selectData = conn -> {
+    private static final ConnectionFunction selectUsers = conn -> {
         String selectSQL = "SELECT username, email FROM users_directory.users";
         try (PreparedStatement selectStatement = conn.prepareStatement(selectSQL)) {
             try (ResultSet resultSet = selectStatement.executeQuery()) {
@@ -63,12 +66,12 @@ public class Main {
     public static void main(String[] args) {
         DbUtils.initializeDatabase(new DataSourceProvider());
 
-        DbUtils.executeStatementsInTransaction(insertDataWithNestedTransaction);
-//        DbUtils.executeStatements(insertData);
-//        DbUtils.executeStatementsInTransaction(insertData);
-//        Object updatedRowsCount = DbUtils.executeStatementsInTransactionWithResult(updateData);
+        DbUtils.executeStatementsInTransaction(insertUsersWithNestedTransaction);
+//        DbUtils.executeStatements(insertUsers);
+//        DbUtils.executeStatementsInTransaction(insertUsers);
+//        Object updatedRowsCount = DbUtils.executeStatementsInTransactionWithResult(updateUserEmails);
 //        System.out.println("Number of Statements Updated: " + updatedRowsCount);
-        Object usersSet = DbUtils.executeStatementsInTransactionWithResult(selectData);
+        Object usersSet = DbUtils.executeStatementsInTransactionWithResult(selectUsers);
         for (String [] userInfo: (HashSet<String[]>) usersSet){
             System.out.println("Username: " + userInfo[0] + "\tEmail: " + userInfo[1]);
         }
