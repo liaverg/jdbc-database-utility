2dce9d52a2b131b970d5a6a173db7b3a8c98506d
Lia Vergou <lia.vergou@gmail.com>
2023-11-28 20:25:33 +0200

    Add methods for creating HikariDataSource

--- src/main/java/com/liaverg/DataSourceProvider.java
+++ src/main/java/com/liaverg/DataSourceProvider.java
@@ -1,28 +1,43 @@
 package com.liaverg;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.Set;
 
 public class DataSourceProvider {
     private final String url;
     private final String username;
     private final String password;
 
-    public DataSourceProvider() {
-        PropertiesReader propertiesReader = new PropertiesReader();
-        this.url = propertiesReader.getPropertiesDbUrl();
-        this.username = propertiesReader.getPropertiesUser();
-        this.password = propertiesReader.getPropertiesPassword();
-    }
-
     public DataSourceProvider(String url, String username, String password) {
         this.url = url;
         this.username = username;
         this.password = password;
+    }
+
+    public DataSourceProvider() {
+        url = null;
+        username = null;
+        password = null;
+    }
+
+    public boolean isDataSourceProviderInitialized(){
+        return !(url == null && username == null && password == null);
+    }
+
+    public HikariDataSource createHikariDataSourceFromProperties(){
+        HikariConfig config = new HikariConfig("/datasource.properties");
+        return new HikariDataSource(config);
+    }
+
+    public HikariDataSource createHikariDataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
     }
 
     public DataSource createDataSource() {
