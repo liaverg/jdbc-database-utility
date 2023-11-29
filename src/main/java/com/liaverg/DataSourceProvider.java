eec62821b25995ff6295182049fb07d44049072a
Lia Vergou <lia.vergou@gmail.com>
2023-11-29 21:58:03 +0200

    Replace HikariDataSource with HikariProxyDataSource

--- src/main/java/com/liaverg/DataSourceProvider.java
+++ src/main/java/com/liaverg/DataSourceProvider.java
@@ -2,40 +2,52 @@ package com.liaverg;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+
+import javax.sql.DataSource;
 
 public class DataSourceProvider {
     private final String url;
     private final String username;
     private final String password;
+    private final int leakDetectionThreshold;
 
     public DataSourceProvider(String url, String username, String password) {
         this.url = url;
         this.username = username;
         this.password = password;
+        this.leakDetectionThreshold = 3000;
     }
 
     public DataSourceProvider() {
-        url = null;
-        username = null;
-        password = null;
+        PropertiesReader propertiesReader = new PropertiesReader();
+        this.url = propertiesReader.getJdbcUrl();
+        this.username = propertiesReader.getUser();
+        this.password = propertiesReader.getPassword();
+        this.leakDetectionThreshold = propertiesReader.getLeakDetectionThreshold();
     }
 
-    public boolean isDataSourceProviderInitialized(){
-        return !(url == null && username == null && password == null);
-    }
-
-    public HikariDataSource createHikariDataSourceFromProperties(){
-        HikariConfig config = new HikariConfig("/datasource.properties");
-        return new HikariDataSource(config);
-    }
-
-    public HikariDataSource createHikariDataSource(){
+    public HikariDataSource createHikariDataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(url);
         config.setUsername(username);
         config.setPassword(password);
-        config.setLeakDetectionThreshold(3000);
-        config.setMaximumPoolSize(10);
+        config.setLeakDetectionThreshold(leakDetectionThreshold);
         return new HikariDataSource(config);
     }
+
+    public DataSource createHikariProxyDataSource() {
+        HikariDataSource dataSource = createHikariDataSource();
+        return ProxyDataSourceBuilder
+                .create(dataSource)
+                .name(url)
+                .logQueryBySlf4j(SLF4JLogLevel.INFO)
+                .multiline()
+                .countQuery()
+                .traceMethods()
+                .build();
+    }
+
+
 }
--- src/main/java/com/liaverg/DbUtils.java
+++ src/main/java/com/liaverg/DbUtils.java
@@ -1,21 +1,16 @@
 package com.liaverg;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
 
 public class DbUtils {
-    private static HikariDataSource dataSource;
+    private static DataSource dataSource;
     private static final ThreadLocal<Connection> connection = ThreadLocal.withInitial(() -> null);
     private static final ThreadLocal<Boolean> isTransactionSuccessful = ThreadLocal.withInitial(() -> true);
-    public static final Logger log = LoggerFactory.getLogger(DbUtils.class);
 
     @FunctionalInterface
     public interface ConnectionConsumer {
@@ -28,14 +23,7 @@ public class DbUtils {
     }
 
     public static void initializeDatabase(DataSourceProvider dataSourceProvider) {
-        log.info("Initializing database {}", LocalDateTime.now());
-        if (dataSourceProvider.isDataSourceProviderInitialized()) {
-            log.info("Initialize from fields of datasource");
-            dataSource = dataSourceProvider.createHikariDataSource();
-        } else {
-            log.info("Initialize from properties");
-            dataSource = dataSourceProvider.createHikariDataSourceFromProperties();
-        }
+        dataSource = dataSourceProvider.createHikariProxyDataSource();
         initializeSchema();
     }
 
@@ -43,25 +31,20 @@ public class DbUtils {
         try (Connection connection = dataSource.getConnection()) {
             ScriptUtils.executeSqlScript(connection, new FileSystemResource("src/main/resources/schema.sql"));
         } catch (SQLException e) {
-            log.info("Exception thrown during database connection");
             throw new RuntimeException("error during database connection", e);
         }
     }
 
     public static void executeStatements(ConnectionConsumer consumer) {
-        log.info("Connecting to the database");
         try (Connection connection = dataSource.getConnection()) {
             try {
-                log.info("Executing statements");
                 consumer.accept(connection);
             } catch (SQLException ex) {
-                log.info("Exception thrown during execution");
                 throw new RuntimeException("error during statement execution", ex);
             }
         } catch (SQLException e) {
-            handleConnectionException(e);
+            throw new RuntimeException("Error during database connection", e);
         }
-        log.info("Closed connection to the database");
     }
 
     public static void executeStatementsInTransaction(ConnectionConsumer consumer) {
@@ -72,19 +55,18 @@ public class DbUtils {
                 isOuterTransaction = true;
                 startTransaction();
             }
-
             try {
-                log.info("Executing statements");
                 consumer.accept(connection.get());
             } catch (SQLException ex) {
-                handleTransactionException(ex);
+                isTransactionSuccessful.set(false);
+                throw new RuntimeException("Error during statement execution", ex);
             } finally {
                 if (isOuterTransaction) {
                     completeOuterTransaction();
                 }
             }
         } catch (SQLException e) {
-            handleConnectionException(e);
+            throw new RuntimeException("Error during database connection", e);
         }
     }
 
@@ -96,19 +78,18 @@ public class DbUtils {
                 isOuterTransaction = true;
                 startTransaction();
             }
-
             try {
-                log.info("Executing statements");
                 result = consumer.apply(connection.get());
             } catch (SQLException ex) {
-                handleTransactionException(ex);
+                isTransactionSuccessful.set(false);
+                throw new RuntimeException("Error during statement execution", ex);
             } finally {
                 if (isOuterTransaction) {
                     completeOuterTransaction();
                 }
             }
         } catch (SQLException e) {
-            handleConnectionException(e);
+            throw new RuntimeException("Error during database connection", e);
         }
         return result;
     }
@@ -118,46 +99,24 @@ public class DbUtils {
     }
 
     private static void startTransaction() throws SQLException {
-        log.info("Connecting to the database");
         isTransactionSuccessful.set(true);
         connection.set(dataSource.getConnection());
         connection.get().setAutoCommit(false);
     }
 
     private static void completeOuterTransaction() throws SQLException {
-        log.info("Finally block executed for outer transaction");
         try {
             if (isTransactionSuccessful.get()) {
-                log.info("Committing statements");
                 connection.get().commit();
             } else {
-                log.info("Rolling back statements");
                 connection.get().rollback();
             }
         } finally {
-            releaseResources();
+            if (connection.get() != null) {
+                connection.get().setAutoCommit(true);
+                connection.get().close();
+                connection.remove();
+            }
         }
-    }
-
-    private static void handleTransactionException(SQLException ex) {
-        log.info("Exception thrown during execution");
-        isTransactionSuccessful.set(false);
-        throw new RuntimeException("Error during statement execution", ex);
-    }
-
-    private static void handleConnectionException(SQLException e) {
-        log.info("Exception thrown during database connection");
-        throw new RuntimeException("Error during database connection", e);
-    }
-
-    private static void releaseResources() throws SQLException {
-        log.info("Releasing resources");
-        if (connection.get() != null) {
-            log.info("Closing connection to the database");
-            connection.get().setAutoCommit(true);
-            connection.get().close();
-            connection.remove();
-        }
-        log.info("Resources released");
     }
 }
